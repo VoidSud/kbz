3f19200f4b13ae6cf45bc1f6ac300095e5eec86d
VoidSud <azzino992@gmail.com>
2024-10-05 00:00:04 +0100

    Delete getbins.php

--- getbins.php
+++
@@ -1,7 +0,0 @@
-<?php
-$bins = array_filter(glob('Bins/*'), 'is_dir'); // Get only directories in the Bins folder
-$binNames = array_map('basename', $bins); // Extract directory names
-
-header('Content-Type: application/json');
-echo json_encode($binNames);
-?>
